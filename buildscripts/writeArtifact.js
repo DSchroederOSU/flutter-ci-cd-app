8b9fed07c8c1fe744baf029408c8798c42d43e86
Daniel Schroeder <me@danielschroeder.org>
2020-02-12 09:16:42 -0800

    new js script

--- buildscripts/writeArtifact.js
+++ buildscripts/writeArtifact.js
@@ -1,5 +1,8 @@
 #!/usr/bin/env node
-
 console.log(process.env.FCI_ARTIFACT_LINKS)
 console.log(process.env.FCI_ARTIFACT_LINKS[0])
 console.log(process.env.FCI_ARTIFACT_LINKS[0].url)
+
+console.log(JSON.parse(process.env.FCI_ARTIFACT_LINKS))
+console.log(JSON.parse(process.env.FCI_ARTIFACT_LINKS)[0])
+console.log(JSON.parse(process.env.FCI_ARTIFACT_LINKS)[0].url)
